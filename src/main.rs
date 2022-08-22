4c2280be3c32af4c453b84503fdde00ea9027e28
it0946 <it@000964@gmail.com>
2022-08-22 18:44:17 +0300

    Rewrite to consume less ram

--- src/main.rs
+++ src/main.rs
@@ -1,274 +1,185 @@
-use anyhow::Context;
-use futures::stream::StreamExt;
-use serde::{Deserialize, Serialize};
-use std::{
-    error::Error,
-    fs,
-    io::{BufReader, Read},
-    path::PathBuf,
-    process::ExitCode,
-    sync::{Arc, RwLock},
+use anyhow::Context as _;
+use serenity::{
+    model::prelude::{AttachmentType, Message, Ready, UserId},
+    prelude::{Context, EventHandler, GatewayIntents},
+    Client,
 };
-use twilight_gateway::{cluster::Events, Cluster, Event};
-use twilight_http::Client as HttpClient;
-use twilight_model::{
-    gateway::{payload::incoming::MessageCreate, Intents},
-    http::attachment::Attachment,
-    id::{marker::UserMarker, Id},
-};
+use std::sync::RwLock;
+use std::{fs, io::ErrorKind, path::PathBuf};
 
-type Image = (String, Vec<u8>);
-type Images = RwLock<Vec<(String, Vec<u8>)>>;
-type HandlerError = Result<(), Box<dyn Error + Send + Sync>>;
-
-const IMAGES_DIR: &str = "./images";
+const IMAGES_PATH: &str = "./images";
 const CONFIG_PATH: &str = "./pittie_config.json";
 
-lazy_static::lazy_static! {
-    static ref CONFIG: Config = Config::init_config(CONFIG_PATH);
+#[derive(serde::Deserialize, serde::Serialize)]
+struct Config {
+    token: String,
+    prefix: String,
+    admins: Vec<UserId>,
 }
 
-struct EventHandler {
-    http: HttpClient,
-    images: Images,
-}
+impl Config {
+    fn init(path: &str) -> anyhow::Result<Option<Self>> {
+        let config: Self = match fs::File::open(path) {
+            Ok(ok) => serde_json::from_reader(ok).with_context(|| "Parsing config failed")?,
+            Err(e) if ErrorKind::NotFound == e.kind() => {
+                let default_config = Config {
+                    token: "Insert your token here".into(),
+                    prefix: "%".into(),
+                    admins: vec![],
+                };
 
-impl EventHandler {
-    fn new(http: HttpClient, images: Images) -> Arc<Self> {
-        Arc::new(Self { http, images })
+                let f = fs::File::create(path)?;
+
+                serde_json::to_writer_pretty(f, &default_config)
+                    .with_context(|| "Failed to write default configuration")?;
+
+                return Ok(None);
+            }
+            Err(e) => Err(e)?,
+        };
+
+        Ok(Some(config))
     }
 
-    fn get_random_image(&self) -> Option<Image> {
+    // fn is_admin(&self, id: &UserId) -> bool {
+    //     self.admins.contains(id)
+    // }
+}
+
+struct Pittie2 {
+    config: Config,
+    img_paths: RwLock<Vec<PathBuf>>,
+}
+
+impl Pittie2 {
+    // Returns Ok(None) if the bot hasn't been started yet and the config and image dirs are just created
+    pub fn new() -> anyhow::Result<Option<Self>> {
+        if let Some(config) = Config::init(CONFIG_PATH)? {
+            let mut img_paths = vec![];
+
+            match fs::read_dir(IMAGES_PATH) {
+                Ok(ok) => {
+                    for file in ok {
+                        if let Ok(file) = file {
+                            if is_img(&file.file_name().to_string_lossy()) {
+                                img_paths.push(file.path());
+                            }
+                        }
+                    }
+                }
+                Err(e) if e.kind() == ErrorKind::NotFound => {
+                    fs::create_dir(IMAGES_PATH)?;
+                }
+                Err(e) => Err(e)?,
+            }
+
+            Ok(Some(Self {
+                config,
+                img_paths: RwLock::new(img_paths),
+            }))
+        } else {
+            Ok(None)
+        }
+    }
+
+    fn prefix<'a>(&'a self) -> &'a String {
+        &self.config.prefix
+    }
+
+    async fn run(self) {
+        let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+        let mut client = Client::builder(&self.config.token, intents)
+            .event_handler(self)
+            .await
+            .expect("Failed to create client");
+
+        if let Err(e) = client.start().await {
+            println!("Client error: {:?}", e);
+        }
+    }
+
+    fn get_rand_path(&self) -> Option<PathBuf> {
         Some({
             let read = self
-                .images
+                .img_paths
                 .read()
-                .expect("failed to acquire images read lock");
+                .expect("Failed to acquire img_paths read lock");
 
             if read.is_empty() {
                 return None;
             }
 
-            let i = fastrand::usize(..read.len());
-            (read[i].0.clone(), read[i].1.clone())
+            read[fastrand::usize(..read.len())].clone()
         })
-    }
-
-    fn get_rand_attachment(&self) -> Option<Attachment> {
-        if let Some((filename, file)) = self.get_random_image() {
-            Some(Attachment {
-                description: None,
-                file,
-                filename,
-                id: 0,
-            })
-        } else {
-            None
-        }
-    }
-
-    // TODO
-    async fn process_cmd(self: Arc<Self>, msg: Box<MessageCreate>) -> HandlerError {
-        let args = &msg.content.as_str()[CONFIG.prefix.len()..];
-        let args: Vec<&str> = args.split(" ").collect();
-
-        match args[0] {
-            "pittie" => self.cmd_pittie(&msg).await,
-            // can't download a file without an http client
-            // "addpittie" => self.cmd_add_pittie(&msg, args).await,
-            _ => {
-                eprintln!("Unknown command: {}{}", CONFIG.prefix, args[0]);
-                Ok(())
-            }
-        }
-    }
-
-    async fn cmd_pittie(self: Arc<Self>, msg: &Box<MessageCreate>) -> HandlerError {
-        if let Some(attachment) = self.get_rand_attachment() {
-            self.http
-                .create_message(msg.channel_id)
-                .attachments(&[attachment])?
-                .exec()
-                .await?;
-        } else {
-            self.http
-                .create_message(msg.channel_id)
-                .content("No images provided ):")?
-                .exec()
-                .await?;
-        }
-        Ok(())
-    }
-
-    // TODO
-    // async fn handle_slash_cmd()
-
-    async fn handle_event(self: Arc<Self>, event: Event) -> HandlerError {
-        match event {
-            Event::MessageCreate(msg)
-                if !msg.author.bot && msg.content.starts_with(&CONFIG.prefix) =>
-            {
-                self.process_cmd(msg).await?;
-            }
-            Event::Ready(ready) => {
-                println!(
-                    "Logged in as {}#{:0>4}",
-                    ready.user.name, ready.user.discriminator
-                );
-            }
-            _ => {}
-        }
-
-        Ok(())
     }
 }
 
-// TODO clean shutdown
-struct Pittie {
-    _cluster: Cluster,
-    events: Events,
-    handler: Arc<EventHandler>,
-}
+#[serenity::async_trait]
+impl EventHandler for Pittie2 {
+    async fn message(&self, ctx: Context, msg: Message) {
+        let args: Vec<&str> = msg.content.split(" ").collect();
+        let prefix = self.prefix();
 
-impl Pittie {
-    async fn init() -> anyhow::Result<Self> {
-        let images = Self::get_images()?;
+        if args[0].starts_with(prefix) {
+            let name = &args[0][prefix.len()..];
 
-        // Initialise discord api related stuff
-        let intents = Intents::GUILD_MESSAGES | Intents::MESSAGE_CONTENT;
+            match name {
+                "pittie" => {
+                    // I don't think I need to care if this errors
+                    let _typing = msg.channel_id.start_typing(&ctx.http);
+                    let rand_path = self.get_rand_path();
 
-        let (cluster, events) = Cluster::new(CONFIG.token.clone(), intents)
-            .await
-            .with_context(|| "failed to create cluster (is your token valid?)")?;
-
-        // Start in the background until I need the cluster for the rest of the struct and use the join handle
-        let handle = tokio::spawn(async move {
-            cluster.up().await;
-            cluster
-        });
-
-        let http = HttpClient::new(CONFIG.token.clone());
-
-        let cluster = handle.await?;
-
-        Ok(Self {
-            _cluster: cluster,
-            events,
-            handler: EventHandler::new(http, images),
-        })
-    }
-
-    fn get_images() -> anyhow::Result<Images> {
-        let mut imagev = vec![];
-        let path = PathBuf::from(IMAGES_DIR);
-
-        let dir_iter = match fs::read_dir(&path) {
-            Ok(ok) => ok,
-            Err(e) if e.kind() == std::io::ErrorKind::NotFound => {
-                fs::create_dir(&path).with_context(|| "Couldn't create image dir")?;
-                fs::read_dir(&path).with_context(|| "Couldn't read image dir")?
-            }
-            Err(e) => anyhow::bail!("{}", e),
-        };
-
-        for f in dir_iter {
-            // TODO I'm not entirely sure in which case this can return an error so look into this
-            let f = f?;
-            let filename = f.file_name().to_string_lossy().to_string();
-
-            if supported_type(&filename) {
-                let contents = read_file(path.join(&filename))?;
-                imagev.push((filename, contents));
-            }
-        }
-
-        Ok(RwLock::new(imagev))
-    }
-
-    async fn run(mut self) {
-        while let Some((_, event)) = self.events.next().await {
-            let handler = self.handler.clone();
-            tokio::spawn(async move {
-                if let Err(e) = handler.handle_event(event).await {
-                    eprintln!("An error occurred while handling an event: {:?}", e);
+                    if let Err(e) = msg
+                        .channel_id
+                        .send_message(&ctx.http, |msg| {
+                            if let Some(ref path) = rand_path {
+                                msg.add_file(AttachmentType::Path(path))
+                            } else {
+                                msg.content("No images provided ):")
+                            }
+                        })
+                        .await
+                    {
+                        eprintln!("Failed to run command: {:?}", e);
+                    }
                 }
-            });
+                // TODO add this later
+                "addpittie" => {}
+                _ => eprintln!("Unknown command: {}", name),
+            }
         }
+    }
+
+    async fn ready(&self, _ctx: Context, ready: Ready) {
+        println!("Logged in as {}", ready.user.tag());
     }
 }
 
-// For a single server bot a single thread should be plenty
+// TODO perhaps consider switching to serenitys built-in command framework instead of the current
+
 #[tokio::main(flavor = "current_thread")]
-async fn main() -> ExitCode {
-    match Pittie::init().await {
-        Ok(pittie) => {
-            pittie.run().await;
-            // TODO With my current setup this will never be reached unless my internet dies and the bot dies
-            return ExitCode::SUCCESS;
-        }
-        Err(e) => {
-            eprintln!("Failed to initialize bot: {}", e);
-            return ExitCode::FAILURE;
-        }
-    }
-}
+async fn main() {
+    match Pittie2::new() {
+        Ok(ok) => {
+            if let Some(pittie2) = ok {
+                {
+                    // Unwrap here is safe because no writer has been created yet
+                    let images = pittie2.img_paths.read().unwrap();
+                    if images.is_empty() {
+                        eprintln!("No images found in: {}", IMAGES_PATH);
+                    }
+                }
 
-/// Simple helper which easily allows for adding more formats
-#[inline]
-fn supported_type(filename: &str) -> bool {
-    filename.ends_with(".png") || filename.ends_with(".jpg") || filename.ends_with(".jpeg")
-}
-
-fn read_file(path: PathBuf) -> anyhow::Result<Vec<u8>> {
-    let f = fs::File::open(path)?;
-    let mut br = BufReader::new(f);
-
-    let mut res = vec![];
-    br.read_to_end(&mut res)?;
-
-    Ok(res)
-}
-
-#[derive(Deserialize, Serialize)]
-struct Config {
-    admin_users: Vec<Id<UserMarker>>,
-    prefix: String,
-    token: String,
-}
-
-impl Config {
-    fn init_config(config_path: &str) -> Self {
-        let f = match fs::File::open(config_path) {
-            Ok(ok) => ok,
-            Err(e) if e.kind() == std::io::ErrorKind::NotFound => {
-                println!("No config found, creating a new one.");
-                Self::create_default_config(config_path);
-                std::process::exit(1);
-            }
-            Err(e) => panic!("Failed to open config file: {:?}", e),
-        };
-
-        match serde_json::from_reader(f) {
-            Ok(ok) => ok,
-            Err(_) => {
-                eprintln!("Invalid config, resetting to default");
-                Self::create_default_config(config_path);
-                std::process::exit(1);
+                pittie2.run().await;
+            } else {
+                println!("Created default config");
             }
         }
+        Err(e) => eprintln!("Error while initializing: {}", e),
     }
+}
 
-    fn create_default_config(config_path: &str) {
-        // Unwrap here is "safe", because it would fail anyway
-        let file = fs::File::create(config_path).unwrap();
-
-        let default = Config {
-            admin_users: vec![],
-            prefix: "%".into(),
-            token: "your token here".into(),
-        };
-
-        // Unwrap here is also "safe", because it would fail anyway
-        serde_json::to_writer_pretty(&file, &default).unwrap();
-    }
+fn is_img(s: &str) -> bool {
+    s.ends_with(".png") || s.ends_with(".jpg") || s.ends_with(".jpeg")
 }
